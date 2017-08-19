6e346bc475c58a533c6d598de5987aed7e73b1df
Joe <josephsmith@email.com>
2017-08-20 00:35:50 +0100

    initital add to repo

---
+++ src/oop/demo.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//person
+		
+		// Attributes, variables, adjectives, descriptions 
+		String name ="Joe";
+		String email ="joe@testmail.com";
+		String phone ="086822640"; 
+		
+		// action 
+		System.out.println(name +" is walking"); 
+		System.out.println(name +" is eating"); 		
+		
+		
+		
+		
+	}
+
+}
